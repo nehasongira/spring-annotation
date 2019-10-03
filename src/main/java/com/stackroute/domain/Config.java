6ac983a40e43aac84fc86b2c7d25ed7c999dc121
nehasongira <songiraneha11@gmail.com>
2019-10-03 15:58:01 +0530

    task1

--- src/main/java/com/stackroute/domain/Config.java
+++ src/main/java/com/stackroute/domain/Config.java
@@ -3,7 +3,9 @@ package com.stackroute.domain;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 //@ComponentScan("com.stackroute.domain")
 
 public class Config {
