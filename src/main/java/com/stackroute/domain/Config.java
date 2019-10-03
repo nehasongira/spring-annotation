f6d30665fa65bae1ab0fb6a02c1a5e66df2e651c
nehasongira <songiraneha11@gmail.com>
2019-10-03 15:49:05 +0530

    task4

--- src/main/java/com/stackroute/domain/Config.java
+++ src/main/java/com/stackroute/domain/Config.java
@@ -4,21 +4,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Scope;
 
-//@ComponentScan("com.stackroute.domain")
+@ComponentScan("com.stackroute.domain")
 
 public class Config {
 
-    @Bean
-    @Scope("prototype")
-    public Movie getmovie()
-    {
-        return new Movie();
-    }
-    @Bean
-    public Actor getActor()
-    {
-        return new Actor();
-    }
+//    @Bean
+//    @Scope("prototype")
+//    public Movie getmovie()
+//    {
+//        return new Movie();
+//    }
+//    @Bean
+//    public Actor getActor()
+//    {
+//        return new Actor();
+//    }
 
 
 }
--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,28 +1,69 @@
 package com.stackroute.domain;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Movie {
+public class Movie implements ApplicationContextAware, BeanNameAware,BeanFactoryAware {
 
+@Autowired
+    Actor actor;
+    public BeanFactory getBean() {
+        return bean;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Movie getMoviecontext() {
+        return moviecontext;
+    }
+
+    ApplicationContext context;
+    BeanFactory bean;
+    String name;
+    Movie moviecontext;
+
+
+    public ApplicationContext getContext() {
+        return context;
+    }
 
     public void setActor(Actor actor) {
         this.actor = actor;
     }
 
-    @Autowired
-
-     Actor actor;
-
-    public Movie() {
-
-    }
-
-
     public void print()
     {
         actor.prints();
     }
-}
 
+
+
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.context=applicationContext;
+        moviecontext= (Movie)context.getBean("movie",Movie.class);
+        moviecontext.print();
+    }
+
+    @Override
+    public void setBeanName(String bean) {
+        System.out.println(name);
+    }
+
+    @Override
+    public void setBeanFactory(BeanFactory bean) throws BeansException {
+        this.bean = bean;
+        moviecontext= (Movie)bean.getBean("movie",Movie.class);
+        moviecontext.print();
+    }
+}
